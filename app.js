4d3d943303121cbf91025ff6c6b49ade6d9ce5b6
unknown <ewaantonia@gmail.com>
2018-07-24 12:12:40 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log('hello world!');
+
+var today = Date.now();
+console.log(new Date (today));
+
+var astroworld = document.getElementsByTagName('LI');
+console.log(astroworld);
+
+for(i=0; i<astroworld.length; i++){
+astroworld.style.color = "red"
+}
+
+
+$(document).ready(function(){
+$('.page-header').css('color',function(){
+
+
+}) //close documenmt.ready
